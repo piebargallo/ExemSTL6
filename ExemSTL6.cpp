177a2840d1020e8995a8e457d665385b63f1bc3c
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-07-09 11:15:49 +0200

    Add files via upload

---
+++ ExemSTL6.cpp
@@ -0,0 +1,28 @@
+// CPP program to illustrate
+// Implementation of erase() function
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Driver program
+int main() {
+	
+	vector<int> myvector{ 1, 2, 3, 4, 5 };
+	vector<int>::iterator it1, it2;
+
+	it1 = myvector.begin();
+	it2 = myvector.end();
+	it2--;
+	it2--;
+
+	myvector.erase(it1, it2);
+
+	// Printing the Vector
+	for (auto it = myvector.begin(); it != myvector.end(); ++it)
+		cout << ' ' << *it;
+	
+	return 0;
+
+} // End driver
